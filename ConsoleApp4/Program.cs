1ee369e09d9c044e0c7093163fc9d594d4b4fa8c
illya-bashtovoy <bashtoff@gmail.com>
2023-03-12 15:24:36 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] task = { 5, 12, 3, 8, 9, 2, 4, 10, 1, 7 };
+
+            int мінімум = task.Min(); // отримуємо мінімальне значення в масиві
+            int максимум = task.Max(); // отримуємо максимальне значення в масиві
+            double середнєАрифметичне = task.Average(); // отримуємо середнє арифметичне значення в масиві
+            int сума = task.Sum(); // отримуємо суму значень в масиві
+
+            Console.WriteLine("Мінімум: " + мінімум);
+            Console.WriteLine("Максимум: " + максимум);
+            Console.WriteLine("Середнє арифметичне: " + середнєАрифметичне);
+            Console.WriteLine("Сума: " + сума);
+        }
+    }
+}
